485f30b85d51481724d1deea8e8bc6ef0a57765f
hrafnlogi <krummi3@gmail.com>
2017-05-09 17:28:05 +0000

    old todo removed

--- ClientApp/routes.js
+++ ClientApp/routes.js
@@ -7,7 +7,6 @@ import ProjectView from 'components/project-view'
 export const routes = [
     { path: '/', redirect: '/home', show: false },
     { path: '/home', component: ProjectList, display: 'Home', style: 'glyphicon glyphicon-home' },
-    { path: '/todo', component: TodoList, display: 'Todos', style: 'glyphicon glyphicon-education'},
     { path: '/project/:id', component: ProjectView, name: 'openProject'},
     { path: '/project/:id/settings', component: ProjectSettings, show: false, name: 'projectSettings' },
     { path: '/user/settings', component: UserSettings, show: false, name: 'userSettings' }
